654e5f55302a99ed9647fea857102587f8cbd93b
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-03-09 19:03:14 +0700

    Implementing the arrow can press the pressure plate

--- src/block/WoodenPressurePlate.php
+++ src/block/WoodenPressurePlate.php
@@ -38,6 +38,7 @@ use pocketmine\world\sound\RedstonePowerOffSound;
 use pocketmine\world\sound\RedstonePowerOnSound;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Facing;
+use pocketmine\entity\projectile\Arrow;
 use pocketmine\item\Item;
 
 class WoodenPressurePlate extends SimplePressurePlate implements IRedstoneComponent, ILinkRedstoneWire{
@@ -74,6 +75,11 @@ class WoodenPressurePlate extends SimplePressurePlate implements IRedstoneCompon
         if (!$this->isPressed()) return;
 
         $entities = $this->getPosition()->getWorld()->getNearbyEntities($this->getHitCollision());
+		for ($i = 0; $i < count($entities); $i++) {
+            if ($entities[$i] instanceof Arrow) {// TODO trident activate this
+				return;
+			}
+		}
         if (count($entities) !== 0) {
             $this->getPosition()->getWorld()->scheduleDelayedBlockUpdate($this->getPosition(), 20);
             return;
@@ -92,8 +98,10 @@ class WoodenPressurePlate extends SimplePressurePlate implements IRedstoneCompon
     }
 
     public function onEntityInside(Entity $entity): bool {
-        if ($entity instanceof Player && $entity->isSpectator()) return true;
-
+        if ($entity instanceof Player && $entity->isSpectator() || !$entity instanceof Arrow) {//TODO Trident activate this
+			return true;																				  																				   
+	    }
+	
         $entities = $this->getPosition()->getWorld()->getNearbyEntities($this->getHitCollision());
         if (count($entities) <= 0) return true;
 
