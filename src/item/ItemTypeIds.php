11c272b3bc4469c16a6e4d2e58ae63bb9b379f8c
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-03-10 23:33:50 +0700

    Added some variants minecart

--- src/item/ItemTypeIds.php
+++ src/item/ItemTypeIds.php
@@ -334,8 +334,11 @@ final class ItemTypeIds{
 	public const RECORD_CREATOR = 20295;
 	public const RECORD_CREATOR_MUSIC_BOX = 20296;
 	public const RECORD_PRECIPICE = 20297;
+	public const CHEST_MINECART = 20298;
+	public const HOPPER_MINECART = 20299;
+	public const TNT_MINECART = 20300;
 
-	public const FIRST_UNUSED_ITEM_ID = 20298;
+	public const FIRST_UNUSED_ITEM_ID = 20301;
 
 	private static int $nextDynamicId = self::FIRST_UNUSED_ITEM_ID;
 
