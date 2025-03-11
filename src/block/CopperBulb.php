a221f1768730f5e66c092e32b2b187447d87322c
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-03-11 14:09:16 +0700

    Update CopperBulb.php

--- src/block/CopperBulb.php
+++ src/block/CopperBulb.php
@@ -23,19 +23,25 @@ declare(strict_types=1);
 
 namespace pocketmine\block;
 
+use pocketmine\block\utils\RedstoneComponentTrait;
+use pocketmine\block\utils\IRedstoneComponent;
+use pocketmine\block\utils\PowerHelper;
 use pocketmine\block\utils\CopperMaterial;
 use pocketmine\block\utils\CopperOxidation;
 use pocketmine\block\utils\CopperTrait;
 use pocketmine\block\utils\LightableTrait;
 use pocketmine\block\utils\PoweredByRedstoneTrait;
 use pocketmine\data\runtime\RuntimeDataDescriber;
+use pocketmine\event\block\RedstonePowerUpdateEvent;
+use pocketmine\event\block\RedstoneEvent;
 
-class CopperBulb extends Opaque implements CopperMaterial{
+class CopperBulb extends Opaque implements CopperMaterial, IRedstoneComponent{
 	use CopperTrait;
 	use PoweredByRedstoneTrait;
 	use LightableTrait{
 		describeBlockOnlyState as encodeLitState;
 	}
+	use RedstoneComponentTrait;
 
 	protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
 		$this->encodeLitState($w);
@@ -65,5 +71,44 @@ class CopperBulb extends Opaque implements CopperMaterial{
 		}
 
 		return 0;
+	}
+
+	 public function onPostPlace(): void {
+        if (PowerHelper::isPowered($this) === $this->isPowered()) return;
+
+        $this->setPowered(!$this->isPowered());
+        $this->getPosition()->getWorld()->setBlock($this->getPosition(), $this);
+    }
+
+    public function onScheduledUpdate(): void {
+        $side = PowerHelper::isPowered($this);
+        if ($side) return;
+
+        $this->updatePowered(false);
+    }
+
+    public function onRedstoneUpdate(): void {
+        if (PowerHelper::isPowered($this) === $this->isPowered()) return;
+
+        if ($this->isPowered()) {
+            $this->getPosition()->getWorld()->scheduleDelayedBlockUpdate($this->getPosition(), 4);
+            return;
+        }
+
+        $this->updatePowered(true);
+    }
+
+    protected function updatePowered(bool $powered): void {
+        if ($powered === $this->isPowered()) return;
+
+        if (RedstoneEvent::isCallEvent()) {
+            $event = new RedstonePowerUpdateEvent($this, $powered, $this->isPowered());
+            $event->call();
+            $powered = $event->getNewPowered();
+            if ($powered === $this->isPowered()) return;
+        }
+
+        $this->setPowered($powered);
+        $this->getPosition()->getWorld()->setBlock($this->getPosition(), $this);
 	}
 }
